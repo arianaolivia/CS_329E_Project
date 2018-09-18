3304018a6bd1496abeb6631c696c4b9ad59d4f80
arianaolivia <ari667@yahoo.com>
2018-09-18 12:29:56 -0500

    login page - just display, no function

---
+++ login.php
@@ -0,0 +1,9 @@
+<form action="" enctype="multipart/form-data" method="post">
+    Username:    <input type="text" name="name"><br/>
+    Password:    <input type="text" name="password"> <br/>
+
+</form>
+
+<?php
+
+?>
