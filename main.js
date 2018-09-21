ed06d3d8e25a3589c17523800964dc3a48d7e85d
arianaolivia <ari667@yahoo.com>
2018-09-21 18:59:23 -0500

    added semicolons after all lines

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-
      /** Cryptococcus; setting a cookie that can be pulled on the game page */
      function setCookie(cookieName, cookieValue, expTime){
          var date = new Date();
          date.setTime(date.getTime() + (expTime*24*60*60*1000));
          var expires = "expires=" + date.toUTCString();
-         document.cookie = cookieName + "=" + cookieValue + ";" + expTime + ";path=/"
+         document.cookie = cookieName + "=" + cookieValue + ";" + expTime + ";path=/";
      }
      /** Cryptococcus; geteing a cookie that can be associated with user login */
 
@@ -13,23 +12,23 @@
      function compPick(){
             var randNum =  Math.floor(Math.random() * 3);
 
-            if(randNum == 0){
-                return "Paper"
+            if(randNum === 0){
+                return "Paper";
             }
             if(randNum == 1){
-                return "Scissors"
+                return "Scissors";
             }
             if(randNum == 2){
-                return "Rock"
+                return "Rock";
             }
         }
 
         function winCondition(user,comp){
-            document.write("User picked: " + user + "<br/>")
-            document.write("Computer picked: " + comp + "<br/>")
+            document.write("User picked: " + user + "<br/>");
+            document.write("Computer picked: " + comp + "<br/>");
             
             if (user == "Gun"){
-                document.write('User wins!')
+                document.write('User wins!');
                 addWin();
                 
             }
@@ -38,29 +37,29 @@
                 document.write('No winner, go again bois!')
             }
             if(user == "Paper" && comp =="Rock"){
-                document.write('User wins!')
+                document.write('User wins!');
                 addWin();   
             }
             
             if(user == "Paper" && comp =="Scissors"){
-                document.write('Computer wins!')    
+                document.write('Computer wins!');    
             }
             
             if(user == "Scissors" && comp =="Rock"){
-                document.write('Computer wins!')    
+                document.write('Computer wins!');    
             }
             
             if(user == "Scissors" && comp =="Paper"){
-                document.write('User wins!')
+                document.write('User wins!');
                 addWin();
             }
             
             if(user == "Rock" && comp =="Paper"){
-                document.write('Computer wins!')
+                document.write('Computer wins!');
             }
             
             if(user == "Rock" && comp =="Scissors"){
-                document.write('User wins!')
+                document.write('User wins!');
                 addWin();
             }
 
@@ -68,47 +67,38 @@
 
 
         function userPaper(){
-            var userHand = "Paper"
-            var compHand = compPick()
-            winCondition(userHand,compHand)
+            var userHand = "Paper";
+            var compHand = compPick();
+            winCondition(userHand,compHand);
 
         }
 
         function userScissors(){
-            var userHand = "Scissors"
-            var compHand = compPick()
-            winCondition(userHand,compHand)
+            var userHand = "Scissors";
+            var compHand = compPick();
+            winCondition(userHand,compHand);
         }
 
         function userRock(){
-            var userHand = "Rock"
-            var compHand = compPick()
-            winCondition(userHand,compHand)
+            var userHand = "Rock";
+            var compHand = compPick();
+            winCondition(userHand,compHand);
         }
 
         function userGun(){
-            var userHand = "Gun"
-            var compHand = compPick()
-            winCondition(userHand,compHand)
+            var userHand = "Gun";
+            var compHand = compPick();
+            winCondition(userHand,compHand);
 
         }
 
   
 function addWin(){
         // extract cookie string and isolate number string
-        var x = document.cookie
+        var x = document.cookie;
         x = x.substring(9);
-        x = Number(x)
-        x++
+        x = Number(x);
+        x++;
         x = x.toString();
-        document.cookie = "winCount = " + x        
+        document.cookie = "winCount = " + x;        
     }
-
-
-
-
-
-
-
-
-
