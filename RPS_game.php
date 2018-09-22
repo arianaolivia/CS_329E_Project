c9a724f340c6cbc4a102add3f1212fa37a0fc686
arianaolivia <ari667@yahoo.com>
2018-09-22 03:46:48 -0500

    added logout cookie

--- RPS_game.php
+++ RPS_game.php
@@ -9,29 +9,38 @@
 <body>
 <h1>Rock, Paper, Scissors!</h1>
 
+<!-- check if user is 'admin', redirect to 'admin' page if not -->
+<script>
+    
+</script>
+
 
 <input type="button" onclick="location.href='paper.html'" value="Paper">
 <input type="button" onclick="location.href='scissors.html'" value="Scissors">
 <input type="button" onclick="location.href='rock.html'" value="Rock">
 
-    
+    <!-- cookie is null to start, needs to be set to 0 to start -->
     <script src="main.js"></script>
     <script>
         var wins = document.cookie
         wins = wins.substring(9);
-        wins = Number(wins)
+        wins = Number(wins);
         document.write("Wins = " + wins);
     </script>
-    
 
+<br/>    
+<form method="POST" action="logout.php">
+    <input type="submit" value="Log out">
+</form>
 
+<!--
 <form action="Project_0.php" method="post" enctype="multipart/form-data">
     Select image to upload:
     <input type="file" name="fileToUpload" id="fileToUpload">
     <input type="submit" value="Upload Image" name="submit">
 </form>
 
-<img src="fileToUpload" alt="username" width="50" height="50">
+<img src="fileToUpload" alt="username" width="50" height="50">-->
 
 </body>
 </html>
