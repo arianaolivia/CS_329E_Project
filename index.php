23495d9b4a58bca110f9069519cbc3de94e8db17
arianaolivia <ari667@yahoo.com>
2018-09-23 16:27:39 -0500

    added comments

--- index.php
+++ index.php
@@ -17,16 +17,18 @@
         Pick your poisson!
     </h2>
 
+<!-- cryptococcus: this will check if the person logged in is an admin (using cookies) and will display the gun option if they are -->
 <?php if($_COOKIE["usertype"] == "admin") : ?>
-    <input type="button" onclick="location.href='paper_admin.html'" value="Paper">
-    <input type="button" onclick="location.href='scissors_admin.html'" value="Scissors">
-    <input type="button" onclick="location.href='rock_admin.html'" value="Rock">
+    <input type="button" onclick="location.href='paper.html'" value="Paper">
+    <input type="button" onclick="location.href='scissors.html'" value="Scissors">
+    <input type="button" onclick="location.href='rock.html'" value="Rock">
     <input type="button" onclick="location.href='gun.html'" value="Gun">
 <?php else : ?>
     <input type="button" onclick="location.href='paper.html'" value="Paper">
     <input type="button" onclick="location.href='scissors.html'" value="Scissors">
     <input type="button" onclick="location.href='rock.html'" value="Rock">
 <?php endif; ?>
+
 
     <script src="main.js"></script>
     <script>
@@ -38,17 +40,20 @@
     
 
 <br/>
+<!-- cryptococcus: redirects to log in (makes cookies) and log out (erases all cookies) pages -->
 <input type="button" onclick="location.href='login.php'" value="Log in!">
 <form method="POST" action="logout.php">
     <input type="submit" value="Log out">
 </form>
 
+<!-- cryptococcus: upload image form accessible to all -->
 <form action="upload_image.php" method="post" enctype="multipart/form-data">
     Select image to upload:
     <input type="file" name="fileToUpload" id="fileToUpload">
     <input type="submit" value="Upload Image" name="submit">
 </form>
 
+<!-- cryptococcus: displays image  -->
 <?php $userImage = $_COOKIE["username"]; ?>
 <img src="images/<?php echo $userImage; ?>.jpg">
 
