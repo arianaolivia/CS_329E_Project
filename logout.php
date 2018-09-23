a5bb897c9fe4bb967ab1d020ac805c40e61d0278
arianaolivia <ari667@yahoo.com>
2018-09-23 16:32:57 -0500

    added comments

--- logout.php
+++ logout.php
@@ -1,5 +1,7 @@
 <?php
 
+//cryptococcus: deletes all cookies by making them expired and redirects to the main page
+
 foreach($_COOKIE as $key => $value){
     setCookie($key, $value, time() - 3600);
 }
