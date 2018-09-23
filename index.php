166074887312628bd53a9bbe2f9e52d2772ff2b2
arianaolivia <ari667@yahoo.com>
2018-09-23 02:17:49 -0500

    fixed upload function

--- index.php
+++ index.php
@@ -1,21 +1,9 @@
 <head>
     Welcome to Rock, Paper, Scissors!
-</head>
-<body>
-
-</br>
-<input type="button" onclick="location.href='RPS_game.php'" value="Enter Game?"><br/>
-
-
-</body>
-<html lang="en">
-<head>
     <meta charset="UTF-8">
     <title>Rock, Paper, Scissors</title>
-    
-
-
 </head>
+
 <body>
 <h1>Rock, Paper, Scissors!</h1>
 
@@ -26,7 +14,7 @@
     <!-- cookie is null to start, needs to be set to 0 to start -->
     <script src="main.js"></script>
     <script>
-        var wins = getCookie('winCount')
+        var wins = getCookie('winCount');
         wins = Number(wins);
         document.write("Wins = " + wins);
     </script>
@@ -39,13 +27,14 @@
     <input type="submit" value="Log out">
 </form>
 
-<!--
-<form action="Project_0.php" method="post" enctype="multipart/form-data">
+<form action="upload_image.php" method="post" enctype="multipart/form-data">
     Select image to upload:
     <input type="file" name="fileToUpload" id="fileToUpload">
     <input type="submit" value="Upload Image" name="submit">
 </form>
-<img src="fileToUpload" alt="username" width="50" height="50">-->
+
+<?php $userImage = $_COOKIE["username"]; ?>
+<img src="images/<?php echo $userImage; ?>.jpg">
 
 </body>
 </html>
